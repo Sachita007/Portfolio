c0b32bdeb6b61613dcbc5eaba114387ef289953a
Sachita Nand <shachitanandk@gmail.com>
2024-07-25 21:05:08 +0530

    .

--- src/portfolio.js
+++ src/portfolio.js
@@ -370,7 +370,7 @@ const projects = {
       image: "fakeshop.png",
       github: "https://github.com/Sachita007/FakeShopAPI",
       link: "https://fake-shopapi.netlify.app/",
-      description: "FakeShopAPI is a free online REST API that you can use whenever you need Pseudo-real data for your e-commerce or shopping website without running any server-side code. It's awesome for teaching purposes, sample codes, tests and etc.",
+      description: "FakeShopAPI is a free online REST API that you can use whenever you need Pseudo-real data for your e-commerce or shopping website without running any server-side code.",
 
       languages: [
         {
